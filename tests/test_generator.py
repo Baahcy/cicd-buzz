4a9cb1ec834e806061435635676324c54fc333ca
Cyril <cbaah123@gmail.com>
2020-03-07 21:27:14 +0000

    PEP 8

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -2,18 +2,21 @@ import unittest
 
 from buzz import generator
 
+
 def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
-    assert word in l
+    lad = ('foo', 'bar', 'foobar')
+    word = generator.sample(lad)
+    assert word in lad
+
 
 def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
-    words = generator.sample(l, 2)
+    lad = ('foo', 'bar', 'foobar')
+    words = generator.sample(lad, 2)
     assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
+    assert words[0] in lad
+    assert words[1] in lad
     assert words[0] is not words[1]
+
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
